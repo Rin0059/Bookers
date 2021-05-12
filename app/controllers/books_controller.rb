40ac53fe7d195cfed677f044ac357bb65b4f4034
Rin0059 <fandorin0059@gmail.com>
2021-05-12 13:58:41 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(dook.id)
+
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).params(:title, :category, :body)
   end
 end
